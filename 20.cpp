fcf8789092339877dbedbc379a8a683cb4f261a5
Victor <victordpc@gmail.com>
2017-12-10 13:39:15 +0100

    Voraz

--- 20.cpp
+++ 20.cpp
@@ -6,61 +6,13 @@
 #include <algorithm>
 #include <vector>
 
-//class CuentaOvejas
-//{
-//public:
-//	CuentaOvejas(Mapa const& ma, int filas, int columnas) : marcados(filas, std::vector<bool>(columnas, false)), ovejasBlancas(-1),
-//		filas(filas), columnas(columnas) {
-//
-//		//Recorremos usandos dfs para recorrer el mapa
-//		//Cada llamada efectiva desde aqui descubre una componente conexa del grafo
-//		//El número de ovejas blancas es el numero de componentes conexas del grafo menos uno (el borde)
-//		// Coste O(n) n=número de nodos
-//		for (int i = 0; i < filas; i++) {
-//			for (int j = 0; j < columnas; j++) {
-//				if (!marcados[i][j] && ma[i][j] != 'X') {
-//					++ovejasBlancas;
-//					Punto inicio = Punto(i, j);
-//					dfs(ma, inicio);
-//				}
-//			}
-//		}
-//
-//	};
-//
-//	int numeroOvejasBlancas() {
-//		return ovejasBlancas;
-//	}
-//
-//private:
-//
-//	std::vector<std::vector<bool>> marcados;
-//	size_t ovejasBlancas;
-//	Punto dir[4] = { Punto(1,0), Punto(0,1), Punto(0,-1),Punto(-1,0) };	//vector de direcciones
-//	int filas;
-//	int columnas;
-//
-//	//función dfs
-//	void dfs(Mapa const& G, Punto v) {
-//		marcados[v.first][v.second] = true;
-//		for (int i = 0; i < 4; i++) {
-//			Punto w = Punto(v.first + dir[i].first, v.second + dir[i].second);
-//			if (w.first >= 0 && w.second >= 0 && w.first < filas && w.second < columnas
-//				&& !marcados[w.first][w.second]
-//				&& G[w.first][w.second] != 'X') {
-//				dfs(G, w);
-//			}
-//		}
-//	}
-//};
-
 class ReparaMangueras
 {
 public:
 	// Estrategia:
 	// Poner un parche siempre que haga falta
-	ReparaMangueras(std::vector<size_t> manguera, size_t agujeros, size_t lParche) 
-		:manguera(manguera), agujeros(agujeros), parches(lParche) ,resultado(1)
+	ReparaMangueras(std::vector<size_t> Manguera, size_t agujeros, size_t lParche) 
+		:manguera(Manguera), agujeros(agujeros), parches(lParche) ,resultado(1)
 	{
 		if (agujeros > 1)  {
 			size_t avanzado = manguera[0];
